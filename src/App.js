2f5ab038518a658f9a1d66ae2f975dc571bd6f1c
LuizaAv <l.avetisyan7777@gmail.com>
2020-11-21 15:43:29 +0400

    added basic dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './App.css';
+import Container from 'react-bootstrap/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "Luiza Portfolio",
+      headerLinks: [
+        {title: 'Home', path: "/"},
+        {title: 'About', path: "/about"},
+        {title: 'Contact', path: "/contact"}
+      ], 
+      home: {
+        title: "Don't stop input more efforts  in your study",
+        subtitle: "You  can  checkout  my projects below"
+      },
+      about: {
+        title: "About me"
+      },
+      contact: {
+        title: "Please, be free to ask me your qestions"
+      }
+    }
+  }
+
+  render(){
+    return(
+      <Router>
+        <Container fluid={true}>
+          <p>ghdgdgdgdg</p>
+        </Container>
+      </Router>
+    )
+  }
 }
 
 export default App;
